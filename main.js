e7999400b800df50f023890d8ac1b9481a263d66
Premopfffff <127723063+Premopfffff@users.noreply.github.com>
2023-04-25 21:33:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const downloadBtn = document.getElementById('download1');
-const audio = document.getElementById('audio');
 
-downloadBtn.addEventListener('click', () => {
-  const audioSrc = audio.src;
-  const a = document.createElement('a');
-  a.href = audioSrc;
-  a.download = 'hay Tamanna hame.mp3';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-});
